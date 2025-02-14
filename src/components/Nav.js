e2a7570809a86cc29db1d4c2d4caf7d383a096dc
nika kobaidze <nika.kobaidze@gmail.com>
2025-02-14 23:00:07 +0400

    [ADD] meta description to app

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservation</li>
+                <li>Order Line</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
+}
