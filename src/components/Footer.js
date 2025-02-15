fab157f2e8cdf4734be504620f245998e901b579
nika kobaidze <nika.kobaidze@gmail.com>
2025-02-15 23:37:31 +0400

    [ADD] footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,40 @@ import logo from "../assets/images/logo.png"
 
 export default function Footer() {
     return (
-        <footer>
-            <div className='footer-logo'>
+        <footer className='container flex'>
+            <div className='footer-logo footer-col'>
                 <img src={logo} alt="footer logo" />
             </div>
+            <div className='footer-menu footer-col'>
+                <p className='footer-title'>Navigation</p>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Menu</a></li>
+                    <li><a href="/">Reservations</a></li>
+                    <li><a href="/">Order Online</a></li>
+                    <li><a href="/">Login</a></li>
+                </ul>
+            </div>
+            <div className='footer-contact footer-col'>
+                <p className='footer-title'>Contact</p>
+                <div className='address-line'>
+                    <p>Address</p>
+                    <p>Phone Number</p>
+                    <p>Email</p>
+                </div>
 
-        </footer>
+            </div>
+            <div className='footer-social footer-col'>
+                <p className='footer-title'>Social Media</p>
+                <ul>
+                    <li><a href="/">Facebook</a></li>
+                    <li><a href="/">Instagram</a></li>
+                    <li><a href="/">TikTok</a></li>
+                    <li><a href="/">Twitter</a></li>
+                </ul>
+            </div>
+
+        </footer >
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,14 +13,7 @@ export default function Header() {
                         <img src={logo} />
                     </a>
                 </div>
-                <nav>
-                    <ul className="nav-links">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
+                <Nav />
             </div>
         </header>
     )
