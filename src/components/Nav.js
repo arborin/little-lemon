7fd84448e7b520768ff6df22ca349a885de8a2d7
nika kobaidze <nika.kobaidze@gmail.com>
2025-02-17 22:59:16 +0400

    [ADD] responsive design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,11 @@ import React from 'react'
 export default function Nav() {
     return (
         <nav>
+            <div class="menu-toggle" onclick="toggleMenu()">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <ul className="nav-links">
                 <li><a href="/">Home</a></li>
                 <li><a href="/">About</a></li>
